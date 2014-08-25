7319f6c278edcb04f8bf3cd2423375210ab6e0bc
Sergey S. Betke <sergey.s.betke@yandex.ru>
2014-08-26 01:52:24 +0400

    * первая рабочая редакция репозитория

--- wp-config.php
+++ wp-config.php
@@ -1,83 +1,44 @@
 <?php
-// ===================================================
-// Load database info and local development parameters
-// ===================================================
-if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
+if ( file_exists( __DIR__ . DIRECTORY_SEPARATOR . 'local-config.php' ) ) {
 	define( 'WP_LOCAL_DEV', true );
-	include( dirname( __FILE__ ) . '/local-config.php' );
+	include( __DIR__ . DIRECTORY_SEPARATOR . 'local-config.php' );
 } else {
 	define( 'WP_LOCAL_DEV', false );
-	define( 'DB_NAME', '%%DB_NAME%%' );
-	define( 'DB_USER', '%%DB_USER%%' );
-	define( 'DB_PASSWORD', '%%DB_PASSWORD%%' );
-	define( 'DB_HOST', '%%DB_HOST%%' ); // Probably 'localhost'
-}
 
-// ========================
-// Custom Content Directory
-// ========================
-define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
-define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
+	define( 'DB_HOST', ini_get( 'mysql.default_host' ) );
+	define( 'DB_NAME', 'nicetournovru' );
+	define( 'DB_USER', 'nicetournovru' );
+	define( 'DB_PASSWORD', 'ghbznyjtgentitcndbt' );
 
-// ================================================
-// You almost certainly do not want to change these
-// ================================================
+	ini_set( 'display_errors', false );
+	define( 'WP_DEBUG', false );
+};
+
+define( 'WP_CONTENT_DIR', __DIR__ . DIRECTORY_SEPARATOR . 'wp-content' );
+
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
-// ==============================================================
-// Salts, for security
-// Grab these from: https://api.wordpress.org/secret-key/1.1/salt
-// ==============================================================
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_KEY',         'z nt,zk.,k.' );
+define( 'SECURE_AUTH_KEY',  'z nt,zk.,k.' );
+define( 'LOGGED_IN_KEY',    'z nt,zk.,k.' );
+define( 'NONCE_KEY',        'z nt,zk.,k.' );
 define( 'AUTH_SALT',        'put your unique phrase here' );
 define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
 define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
 define( 'NONCE_SALT',       'put your unique phrase here' );
 
-// ==============================================================
-// Table prefix
-// Change this if you have multiple installs in the same database
-// ==============================================================
 $table_prefix  = 'wp_';
 
-// ================================
-// Language
-// Leave blank for American English
-// ================================
-define( 'WPLANG', '' );
+define( 'WPLANG', 'ru_RU' );
 
-// ===========
-// Hide errors
-// ===========
-ini_set( 'display_errors', 0 );
-define( 'WP_DEBUG_DISPLAY', false );
-
-// =================================================================
-// Debug mode
-// Debugging? Enable these. Can also enable them in local-config.php
-// =================================================================
-// define( 'SAVEQUERIES', true );
-// define( 'WP_DEBUG', true );
-
-// ======================================
-// Load a Memcached config if we have one
-// ======================================
-if ( file_exists( dirname( __FILE__ ) . '/memcached.php' ) )
-	$memcached_servers = include( dirname( __FILE__ ) . '/memcached.php' );
-
-// ===========================================================================================
-// This can be used to programatically set the stage when deploying (e.g. production, staging)
-// ===========================================================================================
-define( 'WP_STAGE', '%%WP_STAGE%%' );
-define( 'STAGING_DOMAIN', '%%WP_STAGING_DOMAIN%%' ); // Does magic in WP Stack to handle staging domain rewriting
-
-// ===================
-// Bootstrap WordPress
-// ===================
-if ( !defined( 'ABSPATH' ) )
+if ( !defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', dirname( __FILE__ ) . '/wp/' );
+};
 require_once( ABSPATH . 'wp-settings.php' );
+
+// define( 'COOKIE_DOMAIN', 'test.www.nice-tour.nov.ru' );
+
+define( 'CONCATENATE_SCRIPTS', false );
+
+?>
