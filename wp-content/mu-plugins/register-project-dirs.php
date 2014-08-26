f421e09a31c72b3bc2bf8c89f8aaf31ab3fae681
Sergey S. Betke <sergey.s.betke@yandex.ru>
2014-08-26 13:33:50 +0400

    * исправил `register-theme-directory.php`

---
+++ wp-content/mu-plugins/register-project-dirs.php
@@ -0,0 +1,11 @@
+<?php
+/* 
+Plugin Name:		WordPress project template settings
+Plugin URI:			https://github.com/IT-Service-WordPress/
+Description:		This plugin only setup directories for WordPress project with separated <code>wp-content</code> folder.
+Version:			1.0.0
+Author:				Sergey S. Betke
+Author URI:			https://github.com/sergey-s-betke
+*/
+
+register_theme_directory( ABSPATH . 'wp-content/themes/' );
