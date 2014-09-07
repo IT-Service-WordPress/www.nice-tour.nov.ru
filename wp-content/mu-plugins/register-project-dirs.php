f30544b3f5dd889a8099a16eea4d453e14094206
Sergey S. Betke <sergey.s.betke@yandex.ru>
2014-09-07 12:25:14 +0400

    * убрал uri

--- wp-content/mu-plugins/register-project-dirs.php
+++ wp-content/mu-plugins/register-project-dirs.php
@@ -1,7 +1,6 @@
 <?php
 /* 
 Plugin Name:		WordPress project template settings
-Plugin URI:			https://github.com/IT-Service-WordPress/
 Description:		This plugin only setup directories for WordPress project with separated <code>wp-content</code> folder.
 Version:			1.0.0
 Author:				Sergey S. Betke
